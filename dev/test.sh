8bfaf2eafc1c898f16202d53f0bfa7ebc5074d7f
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2022-07-19 11:11:07 +0900

    (dev) sequel to c8b83b1a

--- dev/test.sh
+++ dev/test.sh
@@ -1,5 +1,9 @@
 set -e
+LANG=C
 
 cd work
 setup_IC io_bench
-mpirun -np 8 -hostfile ../hosts -map-by node io_bench_argot io_bench-init/io_bench-init io_bench.pars
+mpirun -np 8 -hostfile $HOME/hosts -map-by node io_bench_argot io_bench-init/io_bench-init io_bench.pars
+
+echo see work/io_bench-out and remove work/\{dmp,io_bench-*,io_bench.diag}
+echo OK
