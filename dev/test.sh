cbe3586ed0aa5af246c740aa9398020652fb525e
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2022-09-13 15:29:58 +0900

    (dev) uses 4 containers not 8 containers

--- dev/test.sh
+++ dev/test.sh
@@ -10,14 +10,19 @@ chlist
 cd work
 rm -rf dmp io_bench-* io_bench.diag
 
-#setup_IC io_bench
-#mpirun -np 8 -hostfile $HOME/hosts -map-by node io_bench_argot io_bench-init/io_bench-init io_bench.pars
+PROB="-x 64 -y 64 -z 32 -X 2 -Y 2 -Z 1"
 
-setup_IC -a chfs io_bench
+#setup_IC $PROB io_bench
+#mpirun -np 4 -hostfile $HOME/hosts -map-by node io_bench_argot $PROB io_bench-init/io_bench-init io_bench.pars
+
+echo setup_IC
+setup_IC -a chfs $PROB io_bench
+echo io_bench_io
 mpirun -x CHFS_SERVER -x CHFS_CHUNK_SIZE -x CHFS_BUF_SIZE \
-	-np 8 -hostfile $HOME/hosts -map-by node io_bench_argot -a chfs \
+	-np 4 -hostfile $HOME/hosts -map-by node io_bench_argot -a chfs $PROB \
 	io_bench-init/io_bench-init io_bench.pars
 
+echo chfsctl stop
 chfsctl -h $HOME/hosts stop
 echo see work/io_bench-out and remove work/\{dmp,io_bench-*,io_bench.diag,log}
 echo OK
